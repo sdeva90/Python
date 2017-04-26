8162b5d8b1ac02606fbe530d106dbb9ddc06cda9
sandhya Devarajan <visualizersandhya@gmail.com>
2017-04-26 09:08:44 -0700

    Delete Exponential.py

--- Exponential.py
+++
@@ -1,9 +0,0 @@
-import math
-import sys
-import os
-import random
-n1 = int(input(" Please Enter the First Number: "))
-NA = int(input(" Please Enter the second number: "))
-n2 = n1**NA
-
-print("the result is", n2)
