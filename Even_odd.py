13911dfc36c89669f151359abb90029e4cd0bf83
sandhya Devarajan <visualizersandhya@gmail.com>
2017-04-26 09:08:30 -0700

    Delete Even_odd.py

--- Even_odd.py
+++
@@ -1,8 +0,0 @@
-import sys
-def Factorial(n): # return factorial
-    result = 1
-    for i in range (1,n):
-        result = result * i
-    print "factorial is ",result
-    return result
-print Factorial(10)
