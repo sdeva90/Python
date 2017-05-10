5d3402e60554643e690b1a198a1b8be4b8f4a436
sandhya <visualizersandhya@gmail.com>
2017-05-10 12:58:49 -0700

    data structures

---
+++ string_find.py
@@ -0,0 +1,20 @@
+# Write a program that prompts for a file name, then opens that file and reads through the file, looking for lines of the form:
+# X-DSPAM-Confidence:    0.8475
+# Count these lines and extract the floating point values from each of the lines and compute the average of those values and produce an output as shown below. Do not use the sum() function or a variable named sum in your solution.
+# You can download the sample data at http://www.pythonlearn.com/code/mbox-short.txt when you are testing below enter mbox-short.txt as the file name.
+# Use the file name mbox-short.txt as the file name
+fname = raw_input("Enter file name: ")
+fh = open(fname)
+total = 0
+count = 0
+for line in fh:
+	if line.startswith("X-DSPAM-Confidence:"):
+		a = line.rstrip()
+		b = line.find(":")
+		c = line[19:]
+		d = c.rstrip()
+		e = float(d)
+		total = e + total
+		count = count + 1
+		d = str(total/count)
+print('Average spam confidence: ' + d)
